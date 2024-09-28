ce165ead2702eb7f5512d038ac244d444caad019
GthiN <Petr.belohoubek89@gmail.com>
2024-09-28 23:12:49 +0200

    Zero+bitbang works ?

--- src/GUI/screens/replayScreen.cpp
+++ src/GUI/screens/replayScreen.cpp
@@ -1,4 +1,5 @@
 #include "replayScreen.h"
+#include "modules/RF/CC1101.h"
 
 ReplayScreen::ReplayScreen()
     : screenManager_(ScreenManager::getInstance()) {
@@ -9,5 +10,17 @@ ReplayScreen::~ReplayScreen() {
 }
 
 void ReplayScreen::initialize() {
+    CC1101_CLASS CC1101;
+          Serial.print("Initializing CC1101...");
+
+  if (CC1101.init())
+  {
+    Serial.print("CC1101 initialized.");
+  }
+  else
+  {
+    Serial.print("CC1101 not initialized.");
+  }
+   // Inicializace RF Modules
     screenManager_.createReplayScreen();
 }
