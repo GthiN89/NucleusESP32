d61b2d602a9333e1ff351a4f06d600c2cabc1352
GthiN <Petr.belohoubek89@gmail.com>
2024-10-22 03:29:47 +0200

    flipper files parser

---
+++ src/modules/dataProcessing/dataProcessing.h
@@ -0,0 +1,10 @@
+enum dataProcessing_state {
+    DATA_PROCESS_IDLE,
+    DATA_PROCESS_FILE,
+    DATA_PROCESS_PARSING
+};
+
+// Funkce používající enum jako návratový typ
+dataProcessing_state getDataProcessingState() {
+    return DATA_PROCESS_IDLE;
+}
